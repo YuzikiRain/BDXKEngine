5739e4ded2eecd5b9326b3bfecee47c56b11ff50
BDFFZI <1330958515@qq.com>
2022-09-27 00:53:12 +0800

    开发实例化功能

---
+++ Sources/Runtime/Base/Serialization/ISerializable.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "Importer.h"
+#include "Exporter.h"
+
+namespace BDXKEngine {
+	struct ISerializable {
+		virtual ~ISerializable() = default;
+		virtual void Import(Importer& importer) = 0;
+		virtual void Export(Exporter& exporter) = 0;
+	};
+}
