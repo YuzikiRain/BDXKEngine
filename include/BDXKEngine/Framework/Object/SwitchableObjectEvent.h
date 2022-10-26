25489e12016e7f39d22f7928ef3e86e5a382c411
BDFFZI <1330958515@qq.com>
2022-10-26 18:37:08 +0800

    -

---
+++ include/BDXKEngine/Framework/Object/SwitchableObjectEvent.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+namespace BDXKEngine
+{
+    class SwitchableObject;
+    
+    class AwakeHandler
+    {
+        friend SwitchableObject;
+    public:
+        virtual ~AwakeHandler() = default;
+    protected:
+        virtual void OnAwake() = 0;
+    };
+
+    class DestroyHandler
+    {
+        friend SwitchableObject;
+    public:
+        virtual ~DestroyHandler() = default;
+    protected:
+        virtual void OnDestroy() = 0;
+    };
+	
+    class EnableHandler
+    {
+        friend SwitchableObject;
+    public:
+        virtual ~EnableHandler() = default;
+    protected:
+        virtual void OnEnable() = 0;
+    };
+    class DisableHandler
+    {
+        friend SwitchableObject;
+    public:
+        virtual ~DisableHandler() = default;
+    protected:
+        virtual void OnDisable() = 0;
+    };
+}
