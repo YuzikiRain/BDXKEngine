d06b29313c9cf21d7a1f6c6e1e7893cf1da0cb3b
BDFFZI <1330958515@qq.com>
2022-10-28 00:13:41 +0800

    优先开发编辑器功能

---
+++ include/BDXKEditor/Base/EditorWindow.cpp
@@ -0,0 +1,26 @@
+﻿#include "BDXKEditor/Base/EditorWindow.h"
+
+#include "imgui/imgui.h"
+
+
+namespace BDXKEditor
+{
+    void EditorWindow::Show()
+    {
+        SetIsEnabling(true);
+    }
+
+
+    void EditorWindow::OnGUI()
+    {
+    }
+
+    void EditorWindow::OnDrawGUI()
+    {
+        ImGui::Begin(this->GetTypeID().c_str());
+
+        OnGUI();
+
+        ImGui::End();
+    }
+}
