935e6b4f1f408d76de1d22d58dfc131d1a0aa603
BDFFZI <1330958515@qq.com>
2022-10-28 23:08:29 +0800

    不行了，必须要做反射，不然编辑器扩展没法写

---
+++ include/BDXKEditor/EditorWindow/EditorWindow.cpp
@@ -0,0 +1,25 @@
+﻿#include "imgui/imgui.h"
+#include "EditorWindow.h"
+
+
+namespace BDXKEditor
+{
+    void EditorWindow::Show()
+    {
+        SetIsEnabling(true);
+    }
+
+
+    void EditorWindow::OnGUI()
+    {
+    }
+
+    void EditorWindow::OnDrawGUI()
+    {
+        ImGui::Begin(this->GetTypeID().c_str());
+
+        OnGUI();
+
+        ImGui::End();
+    }
+}
