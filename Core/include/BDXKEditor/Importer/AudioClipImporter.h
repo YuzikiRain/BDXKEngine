614673b011b4ed5fa2a993397a86e688210a9ede
BDFFZI <1330958515@qq.com>
2023-01-11 23:09:11 +0800

    接入了音频播放功能

---
+++ Core/include/BDXKEditor/Importer/AudioClipImporter.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Core/Importer.h"
+
+namespace BDXKEditor
+{
+    class AudioClipImporter : public Importer
+    {
+        ObjectPtrBase ImportAsset(const std::string& filePath) override;
+    };
+
+    CustomReflection(AudioClipImporter)
+    CustomAssetsImporter(wav, AudioClipImporter)
+}
---
+++ Core/include/BDXKEngine/Base/Serializer/Binary/BinaryWriter.cpp
@@ -0,0 +1,34 @@
+﻿#include "BinaryWriter.h"
+
+namespace BDXKEngine
+{
+    bool BinaryWriter::IsImporter()
+    {
+        return false;
+    }
+    void BinaryWriter::Reset(std::string& data)
+    {
+        data = stream.str();
+        stream.str("");
+    }
+    void BinaryWriter::WriteStreamFrom(const char* value, int size)
+    {
+        stream.write(value, size);
+    }
+
+    void BinaryWriter::TransferString(const std::string& value)
+    {
+        int size = static_cast<int>(value.size());
+        const char* data = value.data();
+
+        WriteStreamFrom(size);
+        WriteStreamFrom(data, size);
+    }
+    void BinaryWriter::TransferBytes(const std::vector<char>& value)
+    {
+        int size = static_cast<int>(value.size());
+
+        WriteStreamFrom(size);
+        WriteStreamFrom(value.data(), size);
+    }
+}
